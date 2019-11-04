f7e54652e783d3c5667522bab165e2f655b1d681
unknown <you@example.com>
2019-11-03 19:04:29 -0500

    1 to 7

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
+
 #include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cout<<"hello world\n";
 }
