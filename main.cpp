b9ca10e7987fc85dd225ceef2049967dcc771162
Jorge Rebosio <jorge.rebosio@utec.edu.pe>
2019-11-09 20:05:12 -0500

    add 11

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
-
-#include <iostream>
+#include <cctype>
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <vector>
 #include <algorithm>
+
 
 using namespace std;
 
+int main(){
 
-int main() {
-    cout<<"hello world\n";
-}
+  cout<<"Hola mundo"<<endl;
+};
